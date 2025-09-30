cfbc96ec61cf5e988cb86f82b75e3fc563be4c51
israfil-miya <israfilmiya120@gmail.com>
2025-09-30 14:04:15 +0600

    chg: removed explicit typing

--- apps/schl-api/src/app.controller.ts
+++ apps/schl-api/src/app.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import type { ApiInfoResponse } from './types/app.types';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get()
-    async getInfo(): Promise<ApiInfoResponse> {
+    async getInfo() {
         return await this.appService.getApiInfo();
     }
 }
